d50c1a0fdb829badcca9010e2c22d0ac02f996d2
Kaleb <kalebnolawi@gmail.com>
2025-10-09 11:56:47 +0400

    minor edits

--- exercises.py
+++ exercises.py
@@ -1,8 +1,7 @@
-# exercises.py
 import random
 
 def generate_matching(vocab_list):
-    # returns pairs and shuffled lists
+    
     pairs = [(v['word'], v['translation']) for v in vocab_list]
     engs = [p[0] for p in pairs]
     amhs = [p[1] for p in pairs]
@@ -11,7 +10,7 @@ def generate_matching(vocab_list):
     return engs, amhs
 
 def generate_fill_blank(sentence, target_word):
-    # replace target_word with blank token
+    
     lower = sentence.lower()
     tgt_lower = target_word.lower()
     if tgt_lower in lower:
